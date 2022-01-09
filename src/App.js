0412c731ed0f9887868945afb437c5c0ab917e7a
sajib hossen <sajibhossensa90@gmail.com>
2022-01-09 15:01:22 +0600

    update react fil

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header/Header';
-import Inventory from './components/Inventory/Inventory';
-import NotFound from './components/NotFound/NotFound';
-import OrderReview from './components/OrderReview/OrderReview';
-import PlaceOrder from './components/PlaceOrder/PlaceOrder';
-import Shop from './components/Shop/Shop';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Inventory from "./components/Inventory/Inventory";
+import NotFound from "./components/NotFound/NotFound";
+import OrderReview from "./components/OrderReview/OrderReview";
+import PlaceOrder from "./components/PlaceOrder/PlaceOrder";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Router>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Shop />}></Route>
+          <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/review" element={<OrderReview />}></Route>
+          <Route path="/inventory" element={<Inventory />}></Route>
+          <Route path="/placeorder" element={<PlaceOrder />}></Route>
+          <Route path="/*" element={<PlaceOrder />}></Route>
+        </Routes>
+      </Router>
+
+      {/* <Router>
         <Header></Header>
         <Switch>
           <Route exact path="/">
@@ -32,8 +44,7 @@ function App() {
             <NotFound></NotFound>
           </Route>
         </Switch>
-      </Router>
-
+      </Router> */}
     </div>
   );
 }
